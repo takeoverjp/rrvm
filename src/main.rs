4b82bee7d41a9be9e91c661a3eb7ec6f3cb60499
Kondo Takeo <takeoverjp@gmail.com>
2019-01-06 13:57:15 +0900

    Refactoring.
    - Change variable name.

--- src/main.rs
+++ src/main.rs
@@ -1502,14 +1502,14 @@ mod tests {
             | (opcode as u32);
     }
 
-    fn inst_i_shamt(_imm:u8, _shamt:u8, _rs1:u8, _funct3:u8, _rsd:u8, _opcode:u8) -> u32 {
-        let imm    = _imm    & ((1 << 6) - 1);
+    fn inst_i_shamt(_l_or_a:u8, _shamt:u8, _rs1:u8, _funct3:u8, _rsd:u8, _opcode:u8) -> u32 {
+        let l_or_a = _l_or_a & ((1 << 6) - 1);
         let shamt  = _shamt  & ((1 << 6) - 1);
         let rs1    = _rs1    & ((1 << 5) - 1);
         let funct3 = _funct3 & ((1 << 3) - 1);
         let rsd    = _rsd    & ((1 << 5) - 1);
         let opcode = _opcode & ((1 << 7) - 1);
-        return ((imm as u32)   << (6 + 5 + 3 + 5 + 7))
+        return ((l_or_a as u32)   << (6 + 5 + 3 + 5 + 7))
             | ((shamt as u32)  << (5 + 3 + 5 + 7))
             | ((rs1 as u32)    << (3 + 5 + 7))
             | ((funct3 as u32) << (5 + 7))
