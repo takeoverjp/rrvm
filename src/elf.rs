bed40f2dbda42b525ab2e55af27e1b1f6284cf1f
Kondo Takeo <takeoverjp@gmail.com>
2019-01-09 12:42:44 +0900

    Support fmt::Display for ElfHeader.

--- src/elf.rs
+++ src/elf.rs
@@ -1,23 +1,48 @@
+use std::fmt;
+
 pub struct ElfHeader {
-    e_ident: [u8; 4],
-    e_type: u8,
-    e_machine: u8,
-    e_version: u8,
-    e_entry: u8,
+    ei_magic: [u8; 4],
+    ei_class: u8,
+    ei_data: u8,
+    ei_version: u8,
+    ei_osabi: u8,
+    ei_abiversion: u8,
 }
 
 impl ElfHeader {
     pub fn new(bin: &Vec<u8>) -> ElfHeader {
         ElfHeader {
-            e_ident: [bin[0], bin[1], bin[2], bin[3]],
-            e_type: bin[4],
-            e_machine: bin[5],
-            e_version: bin[6],
-            e_entry: bin[7],
+            ei_magic: [bin[0], bin[1], bin[2], bin[3]],
+            ei_class: bin[4],
+            ei_data: bin[5],
+            ei_version: bin[6],
+            ei_osabi: bin[7],
+            ei_abiversion: bin[8],
         }
+
     }
     pub fn is_elf(self) -> bool {
-        self.e_ident == [0x7f, b'E', b'L', b'F']
+        self.ei_magic == [0x7f, b'E', b'L', b'F']
+    }
+}
+
+impl fmt::Display for ElfHeader {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.ei_magic != [0x7f, b'E', b'L', b'F'] {
+            return Ok(());
+        }
+
+        write!(f, r"== ELF ==
+  ei_class: {}
+  ei_data: {}
+  ei_version: {}
+  ei_osabi: {}
+  ei_abiversion: {}",
+               self.ei_class,
+               self.ei_data,
+               self.ei_version,
+               self.ei_osabi,
+               self.ei_abiversion)
     }
 }
 
@@ -27,7 +52,7 @@ mod test_elf {
 
     #[test]
     fn test_is_elf() {
-        let bin = vec![0x7f, b'E', b'L', b'F', 0, 0, 0, 0];
+        let bin = vec![0x7f, b'E', b'L', b'F', 0, 0, 0, 0, 0];
         let hdr = ElfHeader::new(&bin);
         assert_eq!(true, hdr.is_elf());
     }
