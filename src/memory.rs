72c0f7510fc150dfd1ea7fc63849e55fe810c606
Kondo Takeo <takeoverjp@gmail.com>
2019-01-19 20:01:26 +0900

    Refactoring.
    - Impl litte endian multi byte decode as a function.

--- src/memory.rs
+++ src/memory.rs
@@ -10,30 +10,6 @@ pub struct Memory<'a> {
     entry_point_offset: u64,
 }
 
-fn u8x8_to_u64(x0:u8, x1:u8, x2:u8, x3:u8,
-               x4:u8, x5:u8, x6:u8, x7:u8) -> u64 {
-    (x0 as u64)
-        | ((x1 as u64) << 8)
-        | ((x2 as u64) << 16)
-        | ((x3 as u64) << 24)
-        | ((x4 as u64) << 32)
-        | ((x5 as u64) << 40)
-        | ((x6 as u64) << 48)
-        | ((x7 as u64) << 56)
-}
-
-fn u8x4_to_u32(x0:u8, x1:u8, x2:u8, x3:u8) -> u32 {
-    (x0 as u32)
-        | ((x1 as u32) << 8)
-        | ((x2 as u32) << 16)
-        | ((x3 as u32) << 24)
-}
-
-fn u8x2_to_u16(x0:u8, x1:u8) -> u16 {
-    (x0 as u16)
-        | ((x1 as u16) << 8)
-}
-
 impl<'a> Memory<'a> {
     pub fn new(map: &'a memmap::Mmap, elf: &'a Elf) -> Memory<'a> {
         Memory {
@@ -45,27 +21,32 @@ impl<'a> Memory<'a> {
     }
 
     pub fn ld(&self, addr: u64) -> u64 {
-        let index = self.addr2index(addr);
-        u8x8_to_u64(self.map[index+0], self.map[index+1], self.map[index+2], self.map[index+3],
-                    self.map[index+4], self.map[index+5], self.map[index+6], self.map[index+7])
+        self.addr2data(addr, 8)
     }
 
     pub fn lw(&self, addr: u64) -> u32 {
-        let index = self.addr2index(addr);
-        u8x4_to_u32(self.map[index+0], self.map[index+1], self.map[index+2], self.map[index+3])
+        self.addr2data(addr, 4) as u32
     }
 
     pub fn lh(&self, addr: u64) -> u16 {
-        let index = self.addr2index(addr);
-        u8x2_to_u16(self.map[index+0], self.map[index+1])
+        self.addr2data(addr, 2) as u16
     }
 
     pub fn lb(&self, addr: u64) -> u8 {
-        let index = self.addr2index(addr);
-        self.map[index]
+        self.addr2data(addr, 1) as u8
     }
 
-    fn addr2index(&self, addr: u64) -> usize{
+    fn addr2data(&self, addr: u64, size: u64) -> u64 {
+        let index = self.addr2index(addr);
+        let mut data: u64 = 0;
+        for cnt in 0..size {
+            data |= (self.map[index + cnt as usize] as u64) << (cnt * 8);
+        }
+
+        data
+    }
+
+    fn addr2index(&self, addr: u64) -> usize {
         (addr - self.entry_point_address + self.entry_point_offset) as usize
     }
 }
