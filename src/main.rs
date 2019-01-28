116edf280609caeac9eb3024e7939deda003ef52
Kondo Takeo <takeoverjp@gmail.com>
2019-01-28 12:43:49 +0900

    Impl mulhu.

--- src/main.rs
+++ src/main.rs
@@ -401,6 +401,7 @@ fn handle_muldiv(reg: &mut RegisterFile, funct3: u32, rd: usize, rs1: usize, rs2
     const FUNCT3_MUL    : u32 = 0b000;
     const FUNCT3_MULH   : u32 = 0b001;
     const FUNCT3_MULHSU : u32 = 0b010;
+    const FUNCT3_MULHU  : u32 = 0b011;
 
     match funct3 {
         FUNCT3_MUL => {
@@ -415,6 +416,11 @@ fn handle_muldiv(reg: &mut RegisterFile, funct3: u32, rd: usize, rs1: usize, rs2
         FUNCT3_MULHSU => {
             info!("mulhsu {},{},{}", ABI_NAME[rd], ABI_NAME[rs1], ABI_NAME[rs2]);
             let tmp = ((reg.x[rs1] as i64) as i128).wrapping_mul((reg.x[rs2] as u64) as i128);
+            reg.x[rd] = (tmp >> XLEN) as u64;
+        },
+        FUNCT3_MULHU => {
+            info!("mulhu {},{},{}", ABI_NAME[rd], ABI_NAME[rs1], ABI_NAME[rs2]);
+            let tmp = ((reg.x[rs1] as u64) as i128).wrapping_mul((reg.x[rs2] as u64) as i128);
             reg.x[rd] = (tmp >> XLEN) as u64;
         },
         _ => warn!("{}: {}: unknown funct3 0x{:x}",
