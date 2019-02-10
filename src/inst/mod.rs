dd6eb64cb33fe81a1a8cb0e791892609b28cc581
Kondo Takeo <takeoverjp@gmail.com>
2019-02-10 12:27:03 +0900

    Refactoring.
    - Remove unnecessary pub attribute.

--- src/inst/mod.rs
+++ src/inst/mod.rs
@@ -1,7 +1,7 @@
 pub mod encodings;
 pub use encodings::*;
 
-pub fn inst_r(_funct7:u32, _rs2:u8, _rs1:u8, _funct3:u32, _rd:u8, _opcode:u32) -> u32 {
+fn inst_r(_funct7:u32, _rs2:u8, _rs1:u8, _funct3:u32, _rd:u8, _opcode:u32) -> u32 {
     let funct7 = _funct7 & ((1 <<  7) - 1);
     let rs2    = _rs2    & ((1 <<  5) - 1);
     let rs1    = _rs1    & ((1 <<  5) - 1);
@@ -16,7 +16,7 @@ pub fn inst_r(_funct7:u32, _rs2:u8, _rs1:u8, _funct3:u32, _rd:u8, _opcode:u32) -
         | (opcode as u32);
 }
 
-pub fn inst_i(_imm:u16, _rs1:u8, _funct3:u32, _rd:u8, _opcode:u32) -> u32 {
+fn inst_i(_imm:u16, _rs1:u8, _funct3:u32, _rd:u8, _opcode:u32) -> u32 {
     let imm    = _imm    & ((1 << 12) - 1);
     let rs1    = _rs1    & ((1 <<  5) - 1);
     let funct3 = _funct3 & ((1 <<  3) - 1);
@@ -29,7 +29,7 @@ pub fn inst_i(_imm:u16, _rs1:u8, _funct3:u32, _rd:u8, _opcode:u32) -> u32 {
         | (opcode as u32);
 }
 
-pub fn inst_i_shamt(_l_or_a:u8, _shamt:u8, _rs1:u8, _funct3:u32, _rd:u8, _opcode:u32) -> u32 {
+fn inst_i_shamt(_l_or_a:u8, _shamt:u8, _rs1:u8, _funct3:u32, _rd:u8, _opcode:u32) -> u32 {
     let l_or_a = _l_or_a & ((1 << 6) - 1);
     let shamt  = _shamt  & ((1 << 6) - 1);
     let rs1    = _rs1    & ((1 << 5) - 1);
@@ -44,7 +44,7 @@ pub fn inst_i_shamt(_l_or_a:u8, _shamt:u8, _rs1:u8, _funct3:u32, _rd:u8, _opcode
         | (opcode as u32);
 }
 
-pub fn inst_u(_imm:u32, _rd:u8, _opcode:u32) -> u32 {
+fn inst_u(_imm:u32, _rd:u8, _opcode:u32) -> u32 {
     let imm    = _imm    & ((1 << 20) - 1);
     let rd     = _rd     & ((1 <<  5) - 1);
     let opcode = (_opcode << 2 | 0b11) & ((1 <<  7) - 1);
@@ -53,7 +53,7 @@ pub fn inst_u(_imm:u32, _rd:u8, _opcode:u32) -> u32 {
         | (opcode as u32);
 }
 
-pub fn inst_cr(_funct4:u16, _rd:u8, _rs2:u8, _opcode:u16) -> u16 {
+fn inst_cr(_funct4:u16, _rd:u8, _rs2:u8, _opcode:u16) -> u16 {
     let funct4 = _funct4 & ((1 <<  4) - 1);
     let rd     = _rd     & ((1 <<  5) - 1);
     let rs2    = _rs2    & ((1 <<  5) - 1);
