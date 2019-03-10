19913fcfaf0c9090f4b81c91a2a4e5a6dcc2fe46
Kondo Takeo <takeoverjp@gmail.com>
2019-03-10 09:46:23 +0900

    Move ABI_NAME declaration to inst module.

--- src/main.rs
+++ src/main.rs
@@ -30,12 +30,6 @@ struct Args {
     offset: u64,
 }
 
-static ABI_NAME: [&'static str; 32] = [
-    "zero", "ra", "sp", "gp", "tp", "t0","t1","t2",
-    "s0/fp", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-    "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-    "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"];
-
 struct RegisterFile {
     csr : ControlStatusRegister,
     pc : u64,
